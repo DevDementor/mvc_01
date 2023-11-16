6f323d772af86f655815b469ba2be769ca92b949
LJW <dementor0711@gmail.com>
2023-11-16 17:20:31 +0900

    Initial commit

---
+++ src/test/java/com/example/mvc_01/Mvc01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvc_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
